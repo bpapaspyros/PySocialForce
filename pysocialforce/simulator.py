50d7aca689b11062ffcfbc22b730198610ee32d2
Vaios Papaspyros <b.papaspyros@gmail.com>
2021-08-10 16:49:22 +0200

    Hand-off correct config files in objects

--- pysocialforce/simulator.py
+++ pysocialforce/simulator.py
@@ -44,10 +44,10 @@ class Simulator:
         # TODO: load obstacles from config
         self.scene_config = self.config.sub_config("scene")
         # initiate obstacles
-        self.env = EnvState(obstacles, self.config("resolution", 10.0))
+        self.env = EnvState(obstacles, self.scene_config("resolution", 10.0))
 
         # initiate agents
-        self.peds = PedState(state, groups, self.config)
+        self.peds = PedState(state, groups, self.scene_config)
 
         # construct forces
         self.forces = self.make_forces(self.config)
